d862606a05faf055de79e08e9e6df5d687a2cabb
Musahaf Ali <musahafali43@gmail.com>
2025-12-16 18:54:50 +0500

    added files

---
+++ Class 4/script.js
@@ -0,0 +1,76 @@
+// console.log("Hello World");
+// ******* Assignment Operators *******
+/*
+let age = 130 ;
+console.log(age);
+age -= 34 ;
+console.log(age);
+age -= 90
+console.log(age);
+age *= 3
+console.log(age);
+age /= 6
+console.log(age);
+age %= 2
+console.log(age);
+*/
+// alert("This is text")
+// let num1 = prompt("Enter value of num1 :")
+// let num2 = prompt("Enter value of num2 :")
+// let no1 = Number(num1)
+// let no2 = Number(num2)
+// alert(no1+no2)
+
+
+// 
+/*
+
+Control Structure
+Conditional Statements 
+if
+if else 
+else if 
+
+*/
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+
+// if(condition){
+//     code
+// }
+// let name = "Ali"
+// let operator = "*" 
+// let num1 = 45 ;
+// let num2 = 34;
+// if(operator == "+"){
+//     console.log(num1+num2);    
+// } else {
+//     console.log("Invalid Operator");
+    
+// }
+
+
+
+// if(age >= 18){
+//     console.log("YES YOU CAN ENROLL IN THIS COURSE ");    
+// } else{
+//     console.log("YES YOU CAN not ENROLL IN THIS COURSE ");   
+
+// }
+
+// Logical Operator
+/*
+&& and operator
+|| or operator
+
+
+*/
+
+let username = prompt("Enter username :")
+let password = prompt("Enter password :")
+
+if(username == "alikhan118" && password == "1234"){
+    document.body.style.backgroundColor = "red"
+    document.body.innerHTML = "<h1> you are loged In</h1>"
+}
