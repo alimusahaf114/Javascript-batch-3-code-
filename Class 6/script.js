cf4357c9c38b7c555843e0970d9713a40d08e674
Musahaf Ali <musahafali43@gmail.com>
2025-12-18 19:42:37 +0500

    added

---
+++ Class 6/script.js
@@ -0,0 +1,29 @@
+// console.log("Hello this is js code");
+// let name = "Ali"
+// console.log(`My name is ${name}`);
+
+// check no is even or odd
+
+// if(num%2==0){
+//     console.log("Even");    
+// }
+/*
+querySelector
+textContent
+innerHTML
+*/
+// let paraText = document.querySelector(".third").textContent;
+// console.log(paraText);
+
+// document.querySelector("p").textContent = "This is dynamically assigned content"
+// document.querySelector("h1").textContent = "This is heading"
+// document.querySelector("span").textContent = "This is span"
+// let num = Number(prompt("No")) ;
+// if(num %2 == 0){
+//     document.querySelector("p").textContent = `${num} is Even No`
+// } else{
+//     document.querySelector("p").textContent = `${num} is Odd No`
+// }
+
+// let data = document.querySelector("p").value 
+// console.log(data);
