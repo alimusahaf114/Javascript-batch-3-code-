82bfb991ae971013392310e2050d21425d850a9e
Musahaf Ali <musahafali43@gmail.com>
2025-12-29 19:10:45 +0500

    added all lines of code covered in class 9

---
+++ Class 9/script.js
@@ -0,0 +1,87 @@
+// let i = 5
+// for( ; i >= 1 ; i--){
+//     console.log(i*5);   
+// }
+
+
+// let i = 1 ;
+// do{
+//     console.log(i);
+//     i++    
+// }while(i >= 10)
+// let name = "Ali"
+// let age = 15
+// if(name == "Naveed"){
+//     if(age == 19){
+//             console.log("Trainer"); 
+//     }  else {
+//         console.log("Sorry");
+        
+//     } 
+// }
+
+// for(let i = 1 ; i <= 4 ; i++){
+    
+//     for(let j = 1 ; j <= i ; j++){
+//         console.log(" *");
+//     }
+//     console.log("\n");
+    
+    
+
+// }
+
+// let marks = [56,78,98,56,45]
+// let lang = ["html" , "css" , "js" , "sql" , "php"]
+// let frontEnd = []
+// console.log(lang);
+// console.log(frontEnd);
+
+// for(let i = 0 ; i < lang.length ; i++){
+//     if(lang[i] == "html"){
+//         console.log("You can learn html");        
+//     } else if(lang[i] == "sql"){
+//         console.log("You can learn SQL");
+        
+//     }else{
+//         console.log(lang[i]);        
+//     }    
+// }
+// console.log(marks.length);
+
+// for(let i = 0 ; i < lang.length ; i++){
+//     if(lang[i]=="html"){
+//         frontEnd.push(lang[i])
+//     } else if(lang[i] == "js"){
+//         frontEnd.push(lang[i])
+//     }
+// }
+
+// console.log(frontEnd);
+// console.log(frontEnd[0]);
+// console.log(frontEnd[1]);
+
+// let numbers = [12,23,45,67,54,32]
+// numbers.forEach()
+
+
+// function printLines(){
+// console.log("I am javascript code");
+// console.log("I am javascript code");
+// console.log("I am javascript code");
+// console.log("I am javascript code");
+// console.log("I am javascript code");
+
+// }
+
+// printLines()
+// printLines()
+// printLines()
+// printLines()
+// printLines()
+// printLines()
+// printLines()
+
+
+
+
