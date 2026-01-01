a24a05654fd2939aed63e649346ff5bd4f4e7970
Musahaf Ali <musahafali43@gmail.com>
2026-01-01 17:04:17 +0500

    added class 9 code

---
+++ Class 10/script.js
@@ -0,0 +1,73 @@
+// function sum(a,b){
+//     let sum = a + b
+//     console.log(sum);    
+// }
+
+// sum(4,7)
+// sum(70,6)
+
+// function checkEvenOdd(num){
+//     if(num%2 == 0){
+//         console.log(`${num} is Even Number`);
+//     }else {
+//         console.log(`${num} is Odd Number`);
+//     }
+    
+// }
+// let number = Number(prompt("Enter any Number"))
+// checkEvenOdd(number)
+
+// function sum(a,b){
+//     let sum = a + b
+//     return sum
+// }
+
+// let result = sum(3,6)
+// console.log(result);
+
+
+// prompt("Enter you value")
+
+// const sum = (a,b) => {
+//     let sum = a + b;
+//     console.log(sum);
+    
+// }
+
+// sum(5,3)
+
+let languages = ["html" , "css" , "js" , "php" , "jquary" , "sql"]
+
+let values = languages.filter((i)  => { 
+
+    
+    // i == "php"
+    // if(languages[i] == "php"){
+    //     return i
+    // } else if (languages[i] == "sql"){
+    //     return i
+    // }
+} )
+console.log(values);
+
+// let newArray = []
+// languages.forEach(
+//     (e)=>{
+//        if(e == "js"){
+//         console.log("You are js");        
+//        }else{
+//         console.log(e);
+        
+//        }
+        
+// }
+// )
+
+// console.log(newArray);
+
+// let languages2 = []
+
+// for(let i = 5 ; i >= 0 ; i--){
+//     languages2.push(languages[i])    
+// }
+// console.log(languages2);
